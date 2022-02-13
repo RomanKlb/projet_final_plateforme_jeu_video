b65c3ac1426509f9e8682eed5f3c27017c104af4
jurufola <moulaye.haidara@laposte.net>
2022-02-13 17:32:20 +0100

    Add GameDto

---
+++ src/main/java/fr/orsys/roman/projet_final_plateforme_jeu_video/business/dto/GameDto.java
@@ -0,0 +1,11 @@
+package fr.orsys.roman.projet_final_plateforme_jeu_video.business.dto;
+
+import java.time.LocalDate;
+
+public class GameDto {
+	String name;
+	String description;
+	LocalDate releaseDate;
+	String image;
+	
+}
