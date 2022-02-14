53202d8ddb456fe93ebef6f66fc39a72f0418e3e
jurufola <moulaye.haidara@laposte.net>
2022-02-14 10:27:40 +0100

    fix merge conflict with main

--- src/main/java/fr/orsys/roman/projet_final_plateforme_jeu_video/business/Game.java
+++ src/main/java/fr/orsys/roman/projet_final_plateforme_jeu_video/business/Game.java
@@ -20,28 +20,6 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-package fr.orsys.roman.projet_final_plateforme_jeu_video.business;
-
-import java.time.LocalDate;
-import java.util.List;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
-import org.hibernate.validator.constraints.Length;
-
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.ToString;
 
 @Entity
@@ -52,75 +30,58 @@ import lombok.ToString;
 @ToString
 public class Game {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@NotNull(message = "Le nom du jeu ne peut pas être vide")
-	@NotBlank(message = "Le nom du jeu doit être complété")
-	private String name;
-	
-	@Length(max = 10000, message = "La description est maximum de 10000 caractères")
-	private String description;
-	
-	private LocalDate releaseDate;
-	
-	private String image;
-	
-public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@NotNull(message = "Le nom du jeu ne peut pas être vide")
-	@NotBlank(message = "Le nom du jeu doit être complété")
-	private String name;
-	
-	@Length(max = 10000, message = "La description est maximum de 10000 caractères")
-	private String description;
-	
-	private LocalDate releaseDate;
-	
-	private String image;
-	
-	@ToString.Exclude
-	@OneToMany(mappedBy = "game", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
-	private List<Reviews> reviews;
-	
-	@ManyToOne
-	private Classification classification;
-	
-	@ManyToOne
-	private Genre genre;
-	
-	@ManyToOne
-	private Editor editor;
-	
-	@ManyToOne
-	private Platform platform;
-	
-	@ManyToOne
-	private BusinessModel businessModel;
+    @NotNull(message = "Le nom du jeu ne peut pas être vide")
+    @NotBlank(message = "Le nom du jeu doit être complété")
+    private String name;
 
-	public Game(
-			@NotNull(message = "Le nom du jeu ne peut pas être vide") @NotBlank(message = "Le nom du jeu doit être complété") String name,
-			@Length(max = 10000, message = "La description est maximum de 10000 caractères") String description,
-			LocalDate releaseDate, String image, List<Reviews> reviews, Classification classification, Genre genre,
-			Editor editor, Platform platform, BusinessModel businessModel) {
-		super();
-		this.name = name;
-		this.description = description;
-		this.releaseDate = releaseDate;
-		this.image = image;
-		this.reviews = reviews;
-		this.classification = classification;
-		this.genre = genre;
-		this.editor = editor;
-		this.platform = platform;
-		this.businessModel = businessModel;
-	}
+    @Length(max = 10000, message = "La description est maximum de 10000 caractères")
+    private String description;
+
+    private LocalDate releaseDate;
+
+    private String image;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "game", fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
+    private List<Reviews> reviews;
+
+    @ManyToOne
+    private Classification classification;
+
+    @ManyToOne
+    private Genre genre;
+
+    @ManyToOne
+    private Editor editor;
+
+    @ManyToOne
+    private Platform platform;
+
+    @ManyToOne
+    private BusinessModel businessModel;
+
+    public Game(
+            @NotNull(message = "Le nom du jeu ne peut pas être vide") @NotBlank(message = "Le nom du jeu doit être complété") String name,
+            @Length(max = 10000, message = "La description est maximum de 10000 caractères") String description,
+            LocalDate releaseDate, String image, List<Reviews> reviews, Classification classification, Genre genre,
+            Editor editor, Platform platform, BusinessModel businessModel) {
+        super();
+        this.name = name;
+        this.description = description;
+        this.releaseDate = releaseDate;
+        this.image = image;
+        this.reviews = reviews;
+        this.classification = classification;
+        this.genre = genre;
+        this.editor = editor;
+        this.platform = platform;
+        this.businessModel = businessModel;
+    }
 
 
-	
+
 }
