85f2381cc81a37c9d7b6af3cf3b989457fea14b6
jurufola <moulaye.haidara@laposte.net>
2022-02-14 18:11:50 +0100

    Update GameDto with validation annotations

--- src/main/java/fr/orsys/roman/projet_final_plateforme_jeu_video/business/dto/GameDto.java
+++ src/main/java/fr/orsys/roman/projet_final_plateforme_jeu_video/business/dto/GameDto.java
@@ -3,6 +3,9 @@ package fr.orsys.roman.projet_final_plateforme_jeu_video.business.dto;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,13 +18,21 @@ import lombok.experimental.FieldDefaults;
 @Getter
 @Setter
 public class GameDto {
+	
+    @NotBlank(message = "Le nom du jeu doit être complété")
 	String name;
+    @NotBlank(message = "La description doit être complétée")
 	String description;
 	LocalDate releaseDate;
 	String image;
+	@NotNull(message = "La classification doit être mentionnée")
 	String classificationName;
+	@NotNull(message = "Le genre doit être mentionné")
 	String genreName;
+	@NotNull(message = "L'editeur doit être mentionné")
 	String editorName;
+	@NotNull(message = "La ou les platefomrmes doit ou doivent être mentionnée")
 	List<String> platformNames;
+	@NotNull(message = "Le modèle économique doit être mentionné")
 	String businessModelName;
 }
--- src/main/java/fr/orsys/roman/projet_final_plateforme_jeu_video/service/BusinessModelService.java
+++ src/main/java/fr/orsys/roman/projet_final_plateforme_jeu_video/service/BusinessModelService.java
@@ -1,5 +1,7 @@
 package fr.orsys.roman.projet_final_plateforme_jeu_video.service;
 
-public interface BusinessModelService {
+import fr.orsys.roman.projet_final_plateforme_jeu_video.business.BusinessModel;
 
+public interface BusinessModelService {
+	BusinessModel addBusinessModel(String businessModelName);
 }
