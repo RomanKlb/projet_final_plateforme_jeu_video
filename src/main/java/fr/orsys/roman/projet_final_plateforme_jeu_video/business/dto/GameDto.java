8afda2d6d0a973eeaf85117d5a039298e035bfc0
jurufola <moulaye.haidara@laposte.net>
2022-02-16 13:49:59 +0100

    New GenreController and New BusinessMModelController

--- src/main/java/fr/orsys/roman/projet_final_plateforme_jeu_video/business/dto/GameDto.java
+++ src/main/java/fr/orsys/roman/projet_final_plateforme_jeu_video/business/dto/GameDto.java
@@ -7,32 +7,38 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
+
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 public class GameDto {
-	 @NotBlank(message = "Le nom du jeu doit être complété")
-		String name;
-	    @NotBlank(message = "La description doit être complétée")
-		String description;
-	    @Past(message = "La date de sortie doit être dans le passée")
-		LocalDate releaseDate;
-		@NotNull(message = "La classification doit être mentionnée")
-		String classificationName;
-		@NotNull(message = "Le genre doit être mentionné")
-		String genreName;
-		@NotNull(message = "L'editeur doit être mentionné")
-		String editorName;
-		@NotNull(message = "La ou les platefomrmes doit ou doivent être mentionnée")
-		List<String> platformNames;
-		@NotNull(message = "Le modèle économique doit être mentionné")
-		String businessModelName;
+	@NotBlank(message = "Le nom du jeu doit être complété")
+	String name;
+	@NotNull(message = "Merci de renseir une description")
+	@NotBlank(message = "La description doit être complétée")
+	String description;
+	@DateTimeFormat(iso = ISO.DATE)
+	@Past(message = "La date de sortie doit être dans le passé")
+	LocalDate releaseDate;
+	@NotNull(message = "La classification doit être mentionnée")
+	String classificationName;
+	@NotNull(message = "Le genre doit être mentionné")
+	String genreName;
+	@NotNull(message = "L'editeur doit être mentionné")
+	String editorName;
+	@NotNull(message = "La ou les platefomrmes doit ou doivent être mentionnée")
+	List<String> platformNames;
+	@NotNull(message = "Le modèle économique doit être mentionné")
+	String businessModelName;
 }
---
+++ src/main/java/fr/orsys/roman/projet_final_plateforme_jeu_video/http/controller/BusinessModelController.java
@@ -0,0 +1,39 @@
+package fr.orsys.roman.projet_final_plateforme_jeu_video.http.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.orsys.roman.projet_final_plateforme_jeu_video.business.BusinessModel;
+import fr.orsys.roman.projet_final_plateforme_jeu_video.business.Editor;
+import fr.orsys.roman.projet_final_plateforme_jeu_video.service.BusinessModelService;
+/**
+ * 
+ * @author Jurufola
+ *
+ */
+@RestController
+@RequestMapping("/businessModel")
+public class BusinessModelController {
+	private final BusinessModelService businessModelService;
+
+	/**
+	 * @param editorService
+	 */
+	public BusinessModelController(BusinessModelService businessModelService) {
+		this.businessModelService = businessModelService;
+	}
+	
+	@GetMapping("/all")
+	public List<BusinessModel> getEditors() {
+		return businessModelService.getAll();
+	}
+	
+	@GetMapping("/{id}")
+	public BusinessModel getBusinessModelById(@PathVariable Long id) {
+		return businessModelService.getById(id);
+	}
+}
