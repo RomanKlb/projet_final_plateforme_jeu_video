912bd567bd5f2f7b53ca284249f7a21f942100e7
jurufola <moulaye.haidara@laposte.net>
2022-02-16 10:27:39 +0100

    Fix pull merge on validation annotations on GameDto

--- src/main/java/fr/orsys/roman/projet_final_plateforme_jeu_video/business/dto/GameDto.java
+++ src/main/java/fr/orsys/roman/projet_final_plateforme_jeu_video/business/dto/GameDto.java
@@ -19,12 +19,20 @@ import lombok.experimental.FieldDefaults;
 @Getter
 @Setter
 public class GameDto {
-	String name;
-	String description;
-	LocalDate releaseDate;
-	String classificationName;
-	String genreName;
-	String editorName;
-	List<String> platformNames;
-	String businessModelName;
+	 @NotBlank(message = "Le nom du jeu doit être complété")
+		String name;
+	    @NotBlank(message = "La description doit être complétée")
+		String description;
+	    @Past(message = "La date de sortie doit être dans le passée")
+		LocalDate releaseDate;
+		@NotNull(message = "La classification doit être mentionnée")
+		String classificationName;
+		@NotNull(message = "Le genre doit être mentionné")
+		String genreName;
+		@NotNull(message = "L'editeur doit être mentionné")
+		String editorName;
+		@NotNull(message = "La ou les platefomrmes doit ou doivent être mentionnée")
+		List<String> platformNames;
+		@NotNull(message = "Le modèle économique doit être mentionné")
+		String businessModelName;
 }
