469fe519a28beb0263d98f486e9263177429fac1
jurufola <moulaye.haidara@laposte.net>
2022-02-20 11:48:33 +0100

    New GameAlreadyExistInDbExecption

---
+++ src/main/java/fr/orsys/roman/projet_final_plateforme_jeu_video/business/exception/existInDB/GameAlreadyExistInDbException.java
@@ -0,0 +1,10 @@
+package fr.orsys.roman.projet_final_plateforme_jeu_video.business.exception.existInDB;
+
+public class GameAlreadyExistInDbException extends AlreadyExistsInDB {
+
+	public GameAlreadyExistInDbException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
