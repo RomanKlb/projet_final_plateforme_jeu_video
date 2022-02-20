fcdc64fb5f51bf88a33dc933cd82cad4a1f5b4d8
jurufola <moulaye.haidara@laposte.net>
2022-02-20 16:06:34 +0100

    fix dupliacte add game OK

--- src/main/java/fr/orsys/roman/projet_final_plateforme_jeu_video/repository/GameRepository.java
+++ src/main/java/fr/orsys/roman/projet_final_plateforme_jeu_video/repository/GameRepository.java
@@ -1,9 +1,12 @@
 package fr.orsys.roman.projet_final_plateforme_jeu_video.repository;
 
+import java.time.LocalDate;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import fr.orsys.roman.projet_final_plateforme_jeu_video.business.Game;
 
 public interface GameRepository extends JpaRepository<Game, Long>{
-
+	//boolean existByNameAndEditorNameAndReleaseDate(String name, String editorName, LocalDate releaseDate);
+	Game findByNameAndEditorNameAndReleaseDate(String name, String editorName, LocalDate releaseDate);
 }
